b938a2cd7708d489b0bfbd0b82461eef28efa4bf
Nat Sl <slupitska@gmail.com>
2022-04-04 00:41:15 +0300

    add my third.3 project

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GradingIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //приложение просит пользователя ввести результат его экзамена
+            //результат экзамена - это одна буква от A до F
+            //A - Outstanding
+            //B - Superior
+            //C - Good
+            //D - Satisfactory
+            //E - Low Pass
+            //F - Failure
+            
+            Console.WriteLine("Enter your exam result:");
+            char userInput = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if(userInput == 'A')
+            {
+                Console.WriteLine("Outstanding!");
+            }
+            if (userInput == 'B')
+            {
+                Console.WriteLine("Superior!");
+            }
+            if (userInput == 'C')
+            {
+                Console.WriteLine("Good!");
+            }
+            if (userInput == 'D')       
+            {
+                Console.WriteLine("Satisfactory.");
+            }
+            if (userInput == 'E')
+            {
+                Console.WriteLine("Low Pass.");
+            }
+            if (userInput == 'F')
+            {
+                Console.WriteLine("Low Pass.");
+            }
+        }
+    }
+}
